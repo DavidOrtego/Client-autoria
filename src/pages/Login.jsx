5471cf51292cb363dafdde5eb01b6a9aa7148281
MT22HUGO <hugomartor07@gmail.com>
2026-04-30 19:01:07 +0200

    Estructura y un poco de logica simple para el Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,17 @@
-const Login = () => {
-  return (
-    <div>
+import React, { useState } from 'react';
+import { Mail, Lock, Eye, EyeOff, Home, ArrowRight, CheckCircle } from 'lucide-react';
 
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="relative min-h-screen flex items-center justify-center p-4 overflow-hidden">
+      
     </div>
   );
 };
+
 
 export default Login;
